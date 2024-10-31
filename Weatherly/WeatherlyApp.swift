5b60516c683b5c145372622195a20adc049dcdd2
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-31 22:48:57 +0100

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Jean on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
